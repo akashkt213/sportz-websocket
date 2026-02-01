1578ec7451454248ba9a095cfcb79187afeb8f0f
Akash Kumar <akashkt213@gmail.com>
2026-02-01 15:13:22 +0530

    added websocket connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import { matchRouter } from './routes/matches.js';
+import http from 'http'
+import { attachWebSocketServer } from './ws/server.js';
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0'
 
 const app = express();
-const PORT = 8000;
+const server = http.createServer(app)
 
 // JSON middleware
 app.use(express.json());
@@ -16,7 +21,12 @@ app.get("/", (req, res) => {
 
 app.use('/matches',matchRouter)
 
+const {broadcastMatchCreated} = attachWebSocketServer(server)
+app.locals.broadcastMatchCreated = broadcastMatchCreated;  //app.locals is express's global object accessible from any request.
+
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+server.listen(PORT,HOST, () => {
+  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(`WebSocket server is running  on ${baseUrl.replace('http','ws')}/ws`);
 });
